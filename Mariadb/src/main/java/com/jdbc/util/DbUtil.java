d78f5a36f48f2507b1af2fd3c24c0b5530991fea
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-19 17:56:04 +0800

    Default Changelist

--- Mariadb/src/main/java/com/jdbc/util/DbUtil.java
+++ Mariadb/src/main/java/com/jdbc/util/DbUtil.java
@@ -1,5 +1,8 @@
 package com.jdbc.util;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,5 +17,22 @@ public class DbUtil {
         String username="root";
         String password="root";
         return DriverManager.getConnection(url, username, password);
+    }
+    private static HikariDataSource dataSource;
+    static {
+        HikariConfig config=new HikariConfig("/hikari.properties");//加载配置文件
+//        HikariConfig config=new HikariConfig();
+//        config.setJdbcUrl("jdbc:mysql://localhost:3306/tedu?serverTimezone=Asia/Shanghai&characterEncoding=utf8");
+//        config.setUsername("root");
+//        config.setPassword("root");
+        dataSource=new HikariDataSource(config);//创建数据源对象（通过此对象获取连接）
+    }
+    public static Connection getHikariConnection()throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public static void main(String[] args) throws SQLException {
+       Connection conn = getHikariConnection();
+        System.out.println(conn);
     }
 }
