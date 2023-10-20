d182c8801a7877efc8d1eaa9b88906388d037dc5
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-20 17:59:48 +0800

    Default Changelist

--- Spring/src/main/java/cn/tedu/spring/example/User.java
+++ Spring/src/main/java/cn/tedu/spring/example/User.java
@@ -2,11 +2,14 @@ package cn.tedu.spring.example;
 
 import org.springframework.stereotype.Component;
 
+import java.awt.*;
+
 @Component
 public class User {
     final String YELLOW = "\u001B[93m";
+    final String RED_COLOR = "\u001B[31m";
     final String RESET_CODE = "\033[0m";
     public void run(){
-        System.out.println(YELLOW+"东方红太阳升 中国出了个毛泽东"+RESET_CODE);
+        System.out.println(YELLOW+"东方红太阳升"+RESET_CODE+RED_COLOR+"中国出了个毛泽东"+RESET_CODE);
     }
 }
