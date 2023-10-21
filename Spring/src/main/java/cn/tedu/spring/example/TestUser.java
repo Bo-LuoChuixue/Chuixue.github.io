90a5225e6c6f3f0a8197503faa98af68de3bf770
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-21 09:55:17 +0800

    Default Changelist

--- Spring/src/main/java/cn/tedu/spring/example/TestUser.java
+++ Spring/src/main/java/cn/tedu/spring/example/TestUser.java
@@ -2,6 +2,7 @@ package cn.tedu.spring.example;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * 测试IoC控制反转【Spring框架创建对象】
@@ -9,9 +10,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class TestUser {
     public static void main(String[] args) {
-        //Spring框架创建对象
+        //Spring框架创建对象【通过注解方式】
         ApplicationContext context=new AnnotationConfigApplicationContext("cn.tedu.spring.example");
-        User user3 = context.getBean(User.class);
-        user3.run();
+        User user1 = context.getBean(User.class);
+        user1.run();
+
+        //Spring框架创建对象【通过xml配置文件方式】
+        ApplicationContext context1=new ClassPathXmlApplicationContext("bean.xml");
+        //User user2= (User) context1.getBean("user");
+        User user2 = context1.getBean(User.class);
+        user2.run();
     }
 }
