6543ad41ebbbdfa74314ea98d5d50341383b0dae
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-23 15:46:18 +0800

    Default Changelist

---
+++ _03MyBatis/src/main/java/cn/tedu/_03mybatis/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package cn.tedu._03mybatis.mapper;
+
+import cn.tedu._03mybatis.pojo.entity.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 1.Mapper注解：MyBatis注解，由框架为该接口创建实现类
+ * 2.Insert注解：MyBatis注解，由框架将SQL语句转为底层的JDBC代码来操作数据库
+ */
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user(username,password,nickname,created) values (#{username},#{password},#{nickname},#{created})")
+    void insertUser(User user);
+}
