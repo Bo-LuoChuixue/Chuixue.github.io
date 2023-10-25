d62852417b3d9fa14e97475f213cfc1952382f83
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-25 16:57:37 +0800

    Default Changelist

--- _04mvcboot01/src/main/java/cn/tedu/_04mvcboot01/controller/UserController.java
+++ _04mvcboot01/src/main/java/cn/tedu/_04mvcboot01/controller/UserController.java
@@ -2,10 +2,12 @@ package cn.tedu._04mvcboot01.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
     @RequestMapping("/v1/users/login")
+    @ResponseBody
     public String login(){
         return "登陆成功";
     }
