697b15484a4dc7f3d65e997399cb99fb5c75fe1f
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-25 17:10:37 +0800

    Default Changelist

--- _04mvcboot01/src/main/java/cn/tedu/_04mvcboot01/controller/UserController.java
+++ _04mvcboot01/src/main/java/cn/tedu/_04mvcboot01/controller/UserController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
+    /**
+     * RequestMapping注解：请求注解
+     * ResponseBody注解：响应注解，允许控制器方法以返回值的形式返回给客户端数据
+     */
     @RequestMapping("/v1/users/login")
     @ResponseBody
     public String login(){
