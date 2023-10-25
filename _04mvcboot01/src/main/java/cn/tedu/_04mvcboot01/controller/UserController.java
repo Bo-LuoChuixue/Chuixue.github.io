812ff5b69a2697a9a0054e38760c67c88d000c3b
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-25 17:51:29 +0800

    Default Changelist

--- _04mvcboot01/src/main/java/cn/tedu/_04mvcboot01/controller/UserController.java
+++ _04mvcboot01/src/main/java/cn/tedu/_04mvcboot01/controller/UserController.java
@@ -7,15 +7,22 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class UserController {
-    /**
-     * RequestMapping注解：请求注解
-     * ResponseBody注解：响应注解，允许控制器方法以返回值的形式返回给客户端数据
-     */
+    /*
+      RequestMapping注解：请求注解
+      ResponseBody注解：响应注解，允许控制器方法以返回值的形式返回给客户端数据
     @RequestMapping("/v1/users/login")
     @ResponseBody
     public String login(HttpServletRequest request){
         String username=request.getParameter("username");
         String password=request.getParameter("password");
+        return username+"："+password;*/
+
+    /*
+        方式二：声明参数方式接收客户端传递的数据
+     */
+    @RequestMapping("/v1/users/login")
+    @ResponseBody
+    public String login(String username,String password) {
         return username+"："+password;
     }
 }
