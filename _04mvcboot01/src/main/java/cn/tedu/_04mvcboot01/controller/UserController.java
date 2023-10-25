623825985d26c62a18fde7450211f3ecb9a713bb
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-25 17:38:37 +0800

    Default Changelist

--- _04mvcboot01/src/main/java/cn/tedu/_04mvcboot01/controller/UserController.java
+++ _04mvcboot01/src/main/java/cn/tedu/_04mvcboot01/controller/UserController.java
@@ -3,7 +3,6 @@ package cn.tedu._04mvcboot01.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
