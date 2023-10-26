ae34e9f6933a5a7f61df5976138b8dacc44ba9dd
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-26 14:17:29 +0800

    Default Changelist

--- _04mvcboot02/src/main/java/cn/tedu/_04mvcboot02/controller/UserController.java
+++ _04mvcboot02/src/main/java/cn/tedu/_04mvcboot02/controller/UserController.java
@@ -1,4 +1,18 @@
 package cn.tedu._04mvcboot02.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class UserController {
+    @RequestMapping("/v1/users/insert")
+    @ResponseBody
+    public String addUser(UserAddDTO userAddDTO){
+        /*
+            1.获取请求体数据；
+            2.将数据插入数据表[MyBatis-接口方法]
+         */
+        return "添加成功";
+    }
 }
