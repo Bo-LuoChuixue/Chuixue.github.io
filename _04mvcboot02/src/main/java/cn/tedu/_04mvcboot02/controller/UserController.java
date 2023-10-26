0cff9e1ea8f275ae6eec2484145971237105f17e
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-26 14:29:57 +0800

    Default Changelist

--- _04mvcboot02/src/main/java/cn/tedu/_04mvcboot02/controller/UserController.java
+++ _04mvcboot02/src/main/java/cn/tedu/_04mvcboot02/controller/UserController.java
@@ -1,5 +1,7 @@
 package cn.tedu._04mvcboot02.controller;
 
+import cn.tedu._04mvcboot02.pojo.dto.UserAddDTO;
+import cn.tedu._04mvcboot02.pojo.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,6 +15,8 @@ public class UserController {
             1.获取请求体数据；
             2.将数据插入数据表[MyBatis-接口方法]
          */
+        //插入数据，一定使用实体类
+        User user=new User();
         return "添加成功";
     }
 }
--- _04mvcboot02/src/main/java/cn/tedu/_04mvcboot02/mapper/UserMapper.java
+++ _04mvcboot02/src/main/java/cn/tedu/_04mvcboot02/mapper/UserMapper.java
@@ -1,4 +1,9 @@
 package cn.tedu._04mvcboot02.mapper;
 
+import cn.tedu._04mvcboot02.pojo.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface UserMapper {
+    int insertUser(User user);
 }
