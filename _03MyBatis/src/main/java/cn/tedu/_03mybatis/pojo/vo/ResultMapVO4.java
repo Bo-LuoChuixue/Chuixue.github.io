ecddf02a04351354b70a4f9a0234b8d418d83bf3
BoluoChuixue <13445933+Boluo-blowing-snow@user.noreply.gitee.com>
2023-10-29 21:28:12 +0800

    Default Changelist

---
+++ _03MyBatis/src/main/java/cn/tedu/_03mybatis/pojo/vo/ResultMapVO4.java
@@ -0,0 +1,43 @@
+package cn.tedu._03mybatis.pojo.vo;
+
+import cn.tedu._03mybatis.pojo.entity.Comment;
+import java.util.List;
+
+public class ResultMapVO4 {
+    private Long id;
+    private String content;
+    private List<Comment> commentList;
+
+    @Override
+    public String toString() {
+        return "ResultMapVO4{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", commentList=" + commentList +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Comment> getCommentList() {
+        return commentList;
+    }
+
+    public void setCommentList(List<Comment> commentList) {
+        this.commentList = commentList;
+    }
+}
