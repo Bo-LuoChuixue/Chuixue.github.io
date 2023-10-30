792f50498a5bfc3f7b297ea3614bea3c9d2e5d6a
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-30 10:10:20 +0800

    Default Changelist

---
+++ _04weibo/src/main/java/cn/tedu/_04weibo/pojo/vo/WeiboInsertVO.java
@@ -0,0 +1,45 @@
+package cn.tedu._04weibo.pojo.vo;
+
+/**
+ * 微博首页列表展示功能的VO类
+ */
+
+public class WeiboInsertVO {
+    //微博id，微博内容，用户昵称
+    private Long id;
+    private String content;
+    private String nickname;
+
+    @Override
+    public String toString() {
+        return "WeiboInsertVO{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", nickname='" + nickname + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
