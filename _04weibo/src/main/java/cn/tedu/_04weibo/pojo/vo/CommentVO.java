04466cddf5f0575700d706e7b064af9013bd928b
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-30 15:04:55 +0800

    Default Changelist

--- _04weibo/src/main/java/cn/tedu/_04weibo/pojo/vo/CommentVO.java
+++ _04weibo/src/main/java/cn/tedu/_04weibo/pojo/vo/CommentVO.java
@@ -2,18 +2,17 @@ package cn.tedu._04weibo.pojo.vo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import javax.xml.crypto.Data;
+import java.util.Date;
 
 /**
  * 评论列表的VO类
  */
-
 public class CommentVO {
-    //评论id，评论内容，评论时间，用户昵称
+    //评论id,评论内容,评论时间,用户昵称
     private Long id;
     private String content;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT-8")
-    private Data created;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date created;
     private String nickname;
 
     @Override
@@ -42,11 +41,11 @@ public class CommentVO {
         this.content = content;
     }
 
-    public Data getCreated() {
+    public Date getCreated() {
         return created;
     }
 
-    public void setCreated(Data created) {
+    public void setCreated(Date created) {
         this.created = created;
     }
 
