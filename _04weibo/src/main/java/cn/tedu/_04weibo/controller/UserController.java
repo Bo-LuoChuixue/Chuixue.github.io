c89753b4d9795ebea83aea3a524bf93468a18418
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-31 16:28:10 +0800

    Default Changelist

--- _04weibo/src/main/java/cn/tedu/_04weibo/controller/UserController.java
+++ _04weibo/src/main/java/cn/tedu/_04weibo/controller/UserController.java
@@ -53,7 +53,7 @@ public class UserController {
         user.setCreated(new Date());
         //调用接口方法
         userMapper.insertUser(user);
-        return new JsonResult(StatusCode.OPERATION_SUCCESS);
+        return JsonResult.ok();
     }
 
     @ApiOperation(value = "登录功能")
