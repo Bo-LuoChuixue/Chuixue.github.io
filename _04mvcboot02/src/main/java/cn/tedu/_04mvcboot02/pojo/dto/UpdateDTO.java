ebee8a2d4956d753007797e7b8aec57196a0b7cc
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-31 11:14:05 +0800

    Default Changelist

--- _04mvcboot02/src/main/java/cn/tedu/_04mvcboot02/pojo/dto/UpdateDTO.java
+++ _04mvcboot02/src/main/java/cn/tedu/_04mvcboot02/pojo/dto/UpdateDTO.java
@@ -1,50 +1,11 @@
 package cn.tedu._04mvcboot02.pojo.dto;
 
+import lombok.Data;
+
+@Data
 public class UpdateDTO {
     private Integer id;
     private String username;
     private String password;
     private String nickname;
-
-    @Override
-    public String toString() {
-        return "UpdateDTO{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", nickname='" + nickname + '\'' +
-                '}';
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
 }
--- _04mvcboot02/src/main/java/cn/tedu/_04mvcboot02/pojo/vo/UserListVO.java
+++ _04mvcboot02/src/main/java/cn/tedu/_04mvcboot02/pojo/vo/UserListVO.java
@@ -1,35 +1,15 @@
 package cn.tedu._04mvcboot02.pojo.vo;
 
+import lombok.Data;
+
 /**
  * 获取用户列表的VO类
  */
+
+@Data
 public class UserListVO {
     private String username;
     private String password;
-
-    @Override
-    public String toString() {
-        return "UserListVO{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
 
 
