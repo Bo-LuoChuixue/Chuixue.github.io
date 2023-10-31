f2c900e6b69a749020acea05cbc18ebe334df760
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-10-31 14:53:09 +0800

    Default Changelist

--- _04weibo/src/main/java/cn/tedu/_04weibo/common/response/StatusCode.java
+++ _04weibo/src/main/java/cn/tedu/_04weibo/common/response/StatusCode.java
@@ -12,7 +12,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public enum StatusCode {
-
+    NOT_LOGIN(1000,"未登录"),
+    LOGIN_SUCCESS(1001,"登陆成功"),
+    PASSWORD_ERROR(1002,"密码错误"),
+    USERNAME_ERROR(1003,"用户名错误"),
+    USERNAME_ALREADY_EXISTS(1004,"用户名已存在"),
+    OPERATION_SUCCESS(2001,"操作成功"),
+    OPERATION_FAILED(2002,"操作失败");
     private Integer code;
     private String msg;
 }
