ec7941e34cef0a07ad4bf2eaaffb68436e761fc0
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-11-01 11:41:21 +0800

    Default Changelist

--- _04weibo/src/main/java/cn/tedu/_04weibo/pojo/dto/UserRegDTO.java
+++ _04weibo/src/main/java/cn/tedu/_04weibo/pojo/dto/UserRegDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * 注册功能DTO类
@@ -16,7 +17,8 @@ public class UserRegDTO {
     //用户名,密码,昵称
     //@NotNull(message = "用户名不能为null")
     //@NotEmpty(message = "用户名不能为null和空字符串")
-    @NotBlank(message = "用户名不能为null和空字符串,也不能为空白串")
+    //@NotBlank(message = "用户名不能为null和空字符串,也不能为空白串")
+    @Size(min=5,max=10,message = "用户名必须在5~10位之间")
     @ApiModelProperty(value = "用户名",required = true,example = "橙留香")
     private String username;
     @ApiModelProperty(value = "密码",required = true,example = "123456")
