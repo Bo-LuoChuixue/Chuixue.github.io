7d9c6ba8a4c177d0d49a023e2f52c1c8d91184d0
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-11-01 10:42:31 +0800

    Default Changelist

--- _04weibo/src/main/java/cn/tedu/_04weibo/pojo/dto/UserRegDTO.java
+++ _04weibo/src/main/java/cn/tedu/_04weibo/pojo/dto/UserRegDTO.java
@@ -2,6 +2,7 @@ package cn.tedu._04weibo.pojo.dto;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.NonNull;
 
 /**
  * 注册功能DTO类
@@ -10,6 +11,7 @@ import lombok.Data;
 @Data
 public class UserRegDTO {
     //用户名,密码,昵称
+    @NonNull
     @ApiModelProperty(value = "用户名",required = true,example = "橙留香")
     private String username;
     @ApiModelProperty(value = "密码",required = true,example = "123456")
