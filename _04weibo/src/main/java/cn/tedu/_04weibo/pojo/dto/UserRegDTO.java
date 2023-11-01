6caa5a3e207377050f65ab07ca4eb93493096f5c
Bo-LuoChuixue <144685977+Bo-LuoChuixue@users.noreply.github.com>
2023-11-01 11:50:00 +0800

    Default Changelist

--- _04weibo/src/main/java/cn/tedu/_04weibo/pojo/dto/UserRegDTO.java
+++ _04weibo/src/main/java/cn/tedu/_04weibo/pojo/dto/UserRegDTO.java
@@ -2,10 +2,8 @@ package cn.tedu._04weibo.pojo.dto;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
+import javax.validation.constraints.*;
 
 /**
  * 注册功能DTO类
@@ -13,15 +11,16 @@ import javax.validation.constraints.Size;
 
 @Data
 public class UserRegDTO {
-    //用户名,密码,昵称
-    //@NotNull(message = "用户名不能为null")
-    //@NotEmpty(message = "用户名不能为null和空字符串")
-    //@NotBlank(message = "用户名不能为null和空字符串,也不能为空白串")
-    @Size(min=5,max=10,message = "用户名必须在5~10位之间")
-    @ApiModelProperty(value = "用户名",required = true,example = "橙留香")
+    //用戶名,密碼,昵稱
+    //@NotNull(message = "用戶名不能為null")
+    //@NotEmpty(message = "用戶名不能為null和空字符串")
+    //@NotBlank(message = "用戶名不能為null和空字符串,也不能為空白串")
+    //@Size(min=5,max=10,message = "用戶名必須在5~10位之間")
+    @Pattern(regexp = "^[\\u4e00-\\u9fa5]{0,7}$|^[a-zA-Z09]{0,14}$",message = "用户名不能超过7个汉字或14个字符")
+    @ApiModelProperty(value = "用戶名",required = true,example = "橙留香")
     private String username;
-    @ApiModelProperty(value = "密码",required = true,example = "123456")
+    @ApiModelProperty(value = "密碼",required = true,example = "123456")
     private String password;
-    @ApiModelProperty(value = "昵称",required = true,example = "小橙")
+    @ApiModelProperty(value = "昵稱",required = true,example = "小橙")
     private String nickname;
 }
